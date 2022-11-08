1e940f276fc93e59060d6af5ced2ad54588ac81a
Chris <chris.m.simpson@icloud.com>
2022-11-09 08:58:11 +1100

    Some basic scaffolding of commands, add zprofile doc

---
+++ Docs/ZPROFILE.md
@@ -0,0 +1,7 @@
+# ~/.zprofile
+
+To have a nice handy `sp` command at your terminal, add the following line to your `~/.zprofile`:
+
+```
+alias sp="~/StreamProcessor/Sources/bin/Debug/net6.0/StreamProcessor $1"
+```
--- Sources/Program.cs
+++ Sources/Program.cs
@@ -3,8 +3,58 @@ namespace StreamProcessor;
 
 public static partial class Program {
 
-    public static void Main(String[] args) {
+    public static String? ValueForKey(
+        this String[] args,
+        String key) {
 
-        WriteLine("hello foo");
+        for (var index = 0; index < args.Length; index++) {
+
+            var arg = args[index];
+
+            if (arg != key) {
+
+                continue;
+            }
+
+            var nextIndex = index + 1;
+
+            if (nextIndex < args.Length) {
+
+                return args[nextIndex];
+            }
+        }
+
+        return null;
+    }
+
+    public static int Main(String[] args) {
+
+        switch (true) {
+
+            case var _ when
+                args.ValueForKey("--read-file") is String file: {
+
+                WriteLine($"TODO: read inline");
+                
+                return 0;
+            }
+
+            case var _ when
+                args.ValueForKey("--read-inline") is String inline: {
+
+                WriteLine($"TODO: read inline");
+
+                return 0;
+            }
+
+            default: {
+
+                WriteLine($"TODO: write usage line");
+
+                return 1;
+            }
+        }
+
+        // WriteLine("hello foo");
     }
 }
