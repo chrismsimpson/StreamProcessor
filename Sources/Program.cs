55c36f43610921caa067e9f0d5f0497ef6405847
Chris <chris.m.simpson@icloud.com>
2022-11-09 09:16:45 +1100

    Some basic raw input

--- Sources/Program.cs
+++ Sources/Program.cs
@@ -27,6 +27,12 @@ public static partial class Program {
         return null;
     }
 
+    public static void WriteErrorLine(
+        String message) {
+
+        WriteLine($"sp: error: {message}");
+    }
+
     public static int Main(String[] args) {
 
         switch (true) {
@@ -34,9 +40,45 @@ public static partial class Program {
             case var _ when
                 args.ValueForKey("--read-file") is String file: {
 
-                WriteLine($"TODO: read inline");
+                if (!File.Exists(file)) {
+
+                    WriteErrorLine("file '{file}' does not exist");
+
+                    return 1;
+                }
+
+                var contents = File.ReadAllText(file);
+
+                var peek = contents.FirstOrDefault();
+
+                switch (peek) {
+
+                    case '[': {
+
+                        var items = JsonSerializer.Deserialize<List<StreamItem>>(contents);
+
+                        return 0;
+                    }
+
+                    case '{': {
+
+                        var item = JsonSerializer.Deserialize<StreamItem>(contents);
+
+                        return 0;
+                    }
+
+                    default: {
+
+                        WriteErrorLine("'{file}' does not appear to be valid json");
+
+                        return 1;
+                    }
+                }
                 
-                return 0;
+
+                // WriteLine($"TODO: read inline");
+                
+                // return 0;
             }
 
             case var _ when
--- Sources/Usings.cs
+++ Sources/Usings.cs
@@ -1,2 +1,6 @@
 
+global using System.IO;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+
 global using static System.Console;
